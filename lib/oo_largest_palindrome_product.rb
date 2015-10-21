dffa015d7bf6a3c83a4a620bb5fe227fa66e86be
Shana L Moore <shana.lavina.moore@gmail.com>
2015-10-20 22:06:53 -0700

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+
+def palindrome?(number)
+  number.to_s == number.to_s.reverse
+end
+
+def answer(range = 100..999)
+  array = []
+  range.each do |x|
+    range.each do |y|
+      array.push(x * y)
+    end
+  end
+  array.select { |n| palindrome?(n) }.max
+end
+
+end
